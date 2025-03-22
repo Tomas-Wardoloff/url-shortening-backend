7e6d77fa283071500e7431152ebb7455f4947547
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-03-22 01:18:40 -0300

    add login method to authController

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -1,30 +1,59 @@
 import { Request, Response } from "express";
-import { signup } from "../services/authService.js";
+import { signup, login } from "../services/authService.js";
 
-async function signupController(request: Request, response: Response) {
+async function signupController(
+  request: Request,
+  response: Response
+): Promise<any> {
   const { firstName, lastName, email, password } = request.body;
 
   if (!firstName || !lastName || !email || !password) {
-    response.status(400).send("Missing required fields");
+    return response.status(400).json({ error: "Missing required fields" });
   }
 
   try {
-    const newUser = await signup(firstName, lastName, email, password);
-    response.status(201).json({ message: "User created", data: newUser });
+    const data = await signup(firstName, lastName, email, password);
+    return response.status(201).json({ message: "User created", data: data });
   } catch (error: any) {
     if (error.message === "User already exists") {
-      response.status(409).json({ error: "User already exists" });
+      return response.status(409).json({ error: error.message });
     }
-    response.status(500).json({ error: "Internal server error" });
+    return response.status(500).json({ error: "Internal server error" });
   }
 }
 
-async function loginController(request: Request, response: Response) {
-  response.send("Login route");
+async function loginController(
+  request: Request,
+  response: Response
+): Promise<any> {
+  const { email, password } = request.body;
+
+  if (!email || !password) {
+    return response.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    const data = await login(email, password);
+    return response.status(200).json({ message: "User logged in", data: data });
+  } catch (error: any) {
+    if (error.message === "User not found") {
+      return response.status(404).json({ error: error.message });
+    }
+    if (error.message === "Invalid password") {
+      return response.status(401).json({ error: error.message });
+    }
+    if (error.message === "User already logged in") {
+      return response.status(409).json({ error: error.message });
+    }
+    return response.status(500).json({ error: "Internal server error" });
+  }
 }
 
-async function logoutController(request: Request, response: Response) {
-  response.send("Logout route");
+async function logoutController(
+  request: Request,
+  response: Response
+): Promise<any> {
+  return response.send("Logout route");
 }
 
 export { signupController, loginController, logoutController };
