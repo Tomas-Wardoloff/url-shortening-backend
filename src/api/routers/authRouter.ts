dab9542a10f38f8c0269d832c7b90e62248f62e2
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-03-23 00:23:46 -0300

    Add authMiddleware to logout and refresh-token routes

--- src/api/routers/authRouter.ts
+++ src/api/routers/authRouter.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
   signupController,
   loginController,
   logoutController,
+  refreshController,
 } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", signupController);
 authRouter.post("/login", loginController);
-authRouter.post("/logout", logoutController);
+authRouter.post("/logout", authMiddleware, logoutController);
+authRouter.post("/refresh-token", authMiddleware, refreshController);
 
 export default authRouter;
