1f93389e08dc69a7da936130e68f68c17a4d1831
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-03-27 21:13:30 -0300

    Add user authorization check to updateUrl method in UrlController

--- src/api/controllers/urlController.ts
+++ src/api/controllers/urlController.ts
@@ -49,6 +49,7 @@ class UrlController {
     response: Response
   ): Promise<any> => {
     // To update the original url and the description
+    const user = request.user;
     const { shortCode } = request.params;
     const { url, description } = request.body;
 
@@ -56,36 +57,21 @@ class UrlController {
       return response.status(400).json({ error: "No data provided" });
 
     try {
-      const data = await this.urlService.updateUrl(shortCode, url, description);
+      const data = await this.urlService.updateUrl(
+        user.id,
+        shortCode,
+        url,
+        description
+      );
       return response.status(200).json({ message: "URL updated", data: data });
     } catch (error: any) {
       if (error.mmessage === "URL not found")
         return response.status(404).json({ error: error.message });
+      else if (error.message === "Action not authorized")
+        return response.status(403).json({ error: error.message });
       return response.status(500).json({ error: error.message });
     }
   };
-
-  public deleteUrl = async (
-    request: AuthRequest,
-    response: Response
-  ): Promise<any> => {
-    /*const user = request.user;
-    const { shortCode } = request.params;
-
-    try {
-      await this.urlService.deleteUrl(shortCode);
-      return response.status(204).json({ message: "URL deleted" });
-    } catch (error: any) {
-      if (error.message === "URL not found")
-        return response.status(404).json({ error: error.message });
-      return response.status(500).json({ error: error.message });
-    }*/
-  };
-
-  public getUserUrls = async (
-    request: AuthRequest,
-    response: Response
-  ): Promise<any> => {};
 }
 
 export default UrlController;
